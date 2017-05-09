1b5a6229797eb92d89649a51d3973b0fba69f435
chasescott <chase.scott.pearson@gmail.com>
2017-05-09 14:54:19 +0100

    Sign in ui custom classes complete

---
+++ claos-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  claos-social
+//
+//  Created by Chase Scott-Pearson on 09/05/2017.
+//  Copyright © 2017 Chase Scott-Pearson. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ claos-social/RoundButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundButton.swift
+//  claos-social
+//
+//  Created by Chase Scott-Pearson on 09/05/2017.
+//  Copyright © 2017 Chase Scott-Pearson. All rights reserved.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        
+        imageView?.contentMode = .scaleAspectFit
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2
+    }
+    
+
+}
