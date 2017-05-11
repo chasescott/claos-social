8cb506f9e1c3d77e13b4c08c967fc912ddfa19ce
chasescott <chase.scott.pearson@gmail.com>
2017-05-11 17:33:48 +0100

    Feed UI set up

--- claos-social/CircleView.swift
+++ claos-social/CircleView.swift
@@ -1,24 +1,27 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  claos-social
 //
-//  Created by Chase Scott-Pearson on 09/05/2017.
+//  Created by Chase Scott-Pearson on 11/05/2017.
 //  Copyright © 2017 Chase Scott-Pearson. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+    }
+    
+    override func layoutSubviews() {
+       super.layoutSubviews()
         
+        layer.cornerRadius = self.frame.width / 2
     }
 
 }
